c4e31983646956758934eff38c907e88064ef951
Odonchimeg Bold <49946963+odmoon@users.noreply.github.com>
2024-05-07 11:49:18 -0500

    Initial project setup with Cookiecutter
    * Initial project setup with Cookiecutter
    
    * Update Readme.md

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Complete MLOPS solution for stock price prediction model',
+    author='Maheen Khan, Dylan Neal, Odonchimeg Bold',
+    license='',
+)
